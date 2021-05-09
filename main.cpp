3a0a3c14c5921af833a6d16bbba834a54840e4ac
ggiap <george.giapotzis@gmail.com>
2021-05-09 10:31:15 -0700

    Removed some gui elements from imgui window

--- main.cpp
+++ main.cpp
@@ -162,7 +162,6 @@ int main()
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     ImVec4 shape_color = ImVec4(0.6f, 1.f, 0.60f, 1.00f);
     static float f = 0.0f;
-    static int counter = 0;
     while (!glfwWindowShouldClose(window))
     {
         // input
@@ -178,16 +177,11 @@ int main()
         {
             ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
 
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Text("Change background and shape colors.");     // Display some text (you can use a format strings too)
 
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
             ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
             ImGui::ColorEdit3("shape color", (float*)&shape_color); // Edit 3 floats representing a color
 
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
 
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             ImGui::End();
